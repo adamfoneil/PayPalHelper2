1ad6fe2dece5303a5a22217e79b4e066956f0669
Adam O'Neil <adamosoftware@gmail.com>
2020-02-10 07:26:44 -0500

    Add project files.

---
+++ PayPalHelper.Core/IpnController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayPalHelper.Core
+{
+    public abstract class IpnController : Controller
+    {
+        private readonly ILogger _logger;
+
+        protected abstract Environment Environment { get; }
+        protected abstract bool LogUnverifiedTransactions { get; }
+
+        protected abstract Task OnVerifiedAsync();
+
+        public IpnController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Listener()
+        {
+            try
+            {
+                if (await Request.IsVerifiedTransaction(Environment))
+                {
+                    _logger.LogInformation($"Verified transaction at {DateTime.UtcNow}: {GetFormText(Request.Form)}");
+
+                    try
+                    {
+                        await OnVerifiedAsync();
+                    }
+                    catch (Exception exc)
+                    {
+                        _logger.LogError(exc, $"Verified transaction handler failed at {DateTime.UtcNow} because {exc.Message}, form data: {GetFormText(Request.Form)}");                        
+                    }                    
+                }
+                else
+                {
+                    if (LogUnverifiedTransactions)
+                    {
+                        _logger.LogInformation($"Unverified transaction at {DateTime.UtcNow}: {GetFormText(Request.Form)}");
+                    }                    
+                }
+            }
+            catch (Exception exc)
+            {
+                _logger.LogError($"Verification check failed at {DateTime.Now}: {exc.Message}");
+            }
+
+            // we never want exceptions getting back to PayPal.
+            // if there's a problem, we must handle internally.
+            return Ok();
+        }
+
+        private string GetFormText(IFormCollection form)
+        {
+            return string.Join("\r\n", form.Select(field => $"{field.Key} = {field.Value}"));
+        }
+    }
+}
---
+++ PayPalHelper.Core/RequestExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PayPalHelper.Core
+{
+    public enum Environment
+    {
+        Sandbox,
+        Live
+    }
+
+    public static class RequestExtensions
+    {
+        private static HttpClient _client = new HttpClient();
+
+        public static async Task<bool> IsVerifiedTransaction(this HttpRequest request, Environment environment)
+        {
+            var urls = new Dictionary<Environment, Uri>
+            {
+                { Environment.Live, new Uri("https://ipnpb.paypal.com/") },
+                { Environment.Sandbox, new Uri("https://ipnpb.sandbox.paypal.com/") }
+            };
+
+            _client.BaseAddress = urls[environment];
+            _client.DefaultRequestHeaders.Accept.Clear();
+
+            var formValues = new Dictionary<string, string>();
+            formValues.Add("cmd", "_notify-validate");
+            foreach (var field in request.Form) formValues.Add(field.Key, field.Value);
+            var content = new FormUrlEncodedContent(formValues);
+
+            var response = await _client.PostAsync("cgi-bin/webscr", content);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return (result.Equals("VERIFIED"));
+            }
+
+            return false;
+
+        }
+    }
+}
